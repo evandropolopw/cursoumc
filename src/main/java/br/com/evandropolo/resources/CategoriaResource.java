9a4304aca8e012b0ee73708514eed5f876ddba65
evandropolopw <evandro@pointware.com.br>
2024-01-31 17:48:38 -0300

    Testando a primeira classe de dominio (categoria)

--- src/main/java/br/com/evandropolo/resources/CategoriaResource.java
+++ src/main/java/br/com/evandropolo/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package br.com.evandropolo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.evandropolo.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!!!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria (1, "Informática");
+		Categoria cat2 = new Categoria (2, "Escritório");
+		
+		List<Categoria> nomeLista = new ArrayList<>();
+		nomeLista.add(cat1);
+		nomeLista.add(cat2);
+				
+		return nomeLista;
 	}
 }
