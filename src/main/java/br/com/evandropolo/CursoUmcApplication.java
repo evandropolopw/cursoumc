4ddcd4ce031fa868af1b56073a316e76bd3771ea
evandropolopw <evandro@pointware.com.br>
2024-01-31 15:38:20 -0300

    Projeto criado

---
+++ src/main/java/br/com/evandropolo/CursoUmcApplication.java
@@ -0,0 +1,13 @@
+package br.com.evandropolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/evandropolo/CursoUmcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evandropolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
