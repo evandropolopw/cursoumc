acaedc70a629bb7e43639be054671c5ec68c2daf
evandropolopw <evandro@pointware.com.br>
2024-02-04 18:28:12 -0300

    Criando operação de instanciação

--- src/main/java/br/com/evandropolo/CursoUmcApplication.java
+++ src/main/java/br/com/evandropolo/CursoUmcApplication.java
@@ -1,13 +1,33 @@
 package br.com.evandropolo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.evandropolo.domain.Categoria;
+import br.com.evandropolo.reposiories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoUmcApplication {
+public class CursoUmcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUmcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
